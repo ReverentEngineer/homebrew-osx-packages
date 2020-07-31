1c644c763117b27142fb450afbd566df285df6b0
Jeff Caffrey-Hill <jeff@reverentengineer.com>
2020-07-31 08:19:04 -0400

    Fix rdg and samba formulas.

--- Formula/rdg.rb
+++ Formula/rdg.rb
@@ -1,9 +1,9 @@
-class RDG < Formula
+class Rdg < Formula
 
   desc "A tool for generating data based on regular expressions"
   homepage "http://reverentengineer.com/rdg"
   url "https://github.com/ReverentEngineer/rdg/archive/v1.0.0.tar.gz"  
-  sha256 "52306326a4107147841da55797ac9c9e3ac391959f1ff78ee6abcdbbd0ba6f25"
+  sha256 "577871d44584f78a335a10cbdf741eb48d6b64ac6a4b435bd53caae3a5240748"
   license "MIT"
   revision 1
 
@@ -11,6 +11,7 @@ class RDG < Formula
   depends_on "cmake" => :build
   depends_on "flex"
   depends_on "bison"
+  depends_on "python3"
   
   def install
     mkdir_p "build"
