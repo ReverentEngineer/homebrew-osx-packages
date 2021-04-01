ab81528f8adf36fca49a480e021d5725d312c0e9
Jeff Caffrey-Hill <jeff@caffreyhill.com>
2021-04-01 16:03:45 -0400

    Add formula to build gcc for arm-none-eabi target

---
+++ Formula/gcc-arm-none-eabi.rb
@@ -0,0 +1,32 @@
+class GccArmNoneEabi < Formula
+
+  desc "gcc for cross-compiling to ARM"
+  homepage "https://www.gnu.org/software/gcc"
+  url "https://ftp.gnu.org/gnu/gcc/gcc-10.2.0/gcc-10.2.0.tar.gz"  
+  sha256 "27e879dccc639cd7b0cc08ed575c1669492579529b53c9ff27b0b96265fa867d"
+  license all_of: ["GPL-2.0-or-later", "GPL-3.0-or-later", "LGPL-2.0-or-later", "LGPL-3.0-only"]
+  revision 1
+
+  def install
+
+	args = %W[
+      --prefix=#{prefix}
+      --target=arm-none-eabi
+      --enable-internetwork
+      --enable-mulitlib
+      --disable-nls
+      --disable-werror
+      --enable-languages=c,c++
+      --without-headers
+      --with-newlib
+      --disable-libssp
+    ]    
+
+    mkdir_p "build"
+    chdir "build"
+    system "../configure",*args
+    system "make"
+    system "make","install" 
+  end
+
+end
